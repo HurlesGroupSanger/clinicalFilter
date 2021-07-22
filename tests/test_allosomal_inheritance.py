18ea32ba5fc6c93b61ed923807011973c4513aa2
RuthEberhardt <re3@sanger.ac.uk>
2021-07-22 11:24:03 +0100

    allosomal inh test set up

--- tests/test_allosomal_inheritance.py
+++ tests/test_allosomal_inheritance.py
@@ -1,4 +1,5 @@
 import unittest
+import copy
 
 from tests.test_utils import create_test_candidate_vars
 from tests.test_utils import create_test_person
@@ -11,12 +12,120 @@ from filtering.inheritance_filtering import InheritanceFiltering
 class TestAllosomalInheritanceFilter(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
+        self.hetvardata = {'chrom': 'X', 'pos': '1097183', 'ref': 'A',
+                           'alt': 'GG',
+                           'consequence': 'start_lost', 'ensg': 'ensg',
+                           'symbol': 'DDX3X', 'feature': 'feature',
+                           'canonical': 'YES', 'mane': 'MANE',
+                           'hgnc_id': '1234', 'ddd_father_af': '.',
+                           'max_af': '0', 'max_af_pops': '.', 'ddd_af': '0',
+                           'revel': '.', 'polyphen': '.', 'hgvsc': '.',
+                           'hgvsp': '.', 'sex': 'XY', 'dnm': '.',
+                           'gt': '0/1', 'gq': '50',
+                           'pid': '.', 'protein_position': '123', 'ad': '4,4'}
+        self.homaltvardata = self.hetvardata.copy()
+        self.homaltvardata['gt'] = '1/1'
+        self.homrefvardata = self.hetvardata.copy()
+        self.homrefvardata['gt'] = '0/0'
 
-    def test_allosomal_no_parents(self):
-        pass
+        self.variants_100 = {'child': {'X_1097183_A_GG': self.hetvardata},
+                             'mum': {'X_1097183_A_GG': self.homrefvardata},
+                             'dad': {'X_1097183_A_GG': self.homrefvardata}}
+        self.variants_110 = {'child': {'X_1097183_A_GG': self.hetvardata},
+                             'mum': {'X_1097183_A_GG': self.hetvardata},
+                             'dad': {'X_1097183_A_GG': self.homrefvardata}}
+        self.variants_102 = {'child': {'X_1097183_A_GG': self.hetvardata},
+                             'mum': {'X_1097183_A_GG': self.homrefvardata},
+                             'dad': {'X_1097183_A_GG': self.homaltvardata}}
+        self.variants_120 = {'child': {'X_1097183_A_GG': self.hetvardata},
+                             'mum': {'X_1097183_A_GG': self.homaltvardata},
+                             'dad': {'X_1097183_A_GG': self.homrefvardata}}
+        self.variants_112 = {'child': {'X_1097183_A_GG': self.hetvardata},
+                             'mum': {'X_1097183_A_GG': self.hetvardata},
+                             'dad': {'X_1097183_A_GG': self.homaltvardata}}
+        self.variants_122 = {'child': {'X_1097183_A_GG': self.hetvardata},
+                             'mum': {'X_1097183_A_GG': self.homaltvardata},
+                             'dad': {'X_1097183_A_GG': self.homaltvardata}}
+        self.variants_200 = {'child': {'X_1097183_A_GG': self.homaltvardata},
+                             'mum': {'X_1097183_A_GG': self.homrefvardata},
+                             'dad': {'X_1097183_A_GG': self.homrefvardata}}
+        self.variants_210 = {'child': {'X_1097183_A_GG': self.homaltvardata},
+                             'mum': {'X_1097183_A_GG': self.hetvardata},
+                             'dad': {'X_1097183_A_GG': self.homrefvardata}}
+        self.variants_202 = {'child': {'X_1097183_A_GG': self.homaltvardata},
+                             'mum': {'X_1097183_A_GG': self.homrefvardata},
+                             'dad': {'X_1097183_A_GG': self.homaltvardata}}
+        self.variants_220 = {'child': {'X_1097183_A_GG': self.homaltvardata},
+                             'mum': {'X_1097183_A_GG': self.homaltvardata},
+                             'dad': {'X_1097183_A_GG': self.homrefvardata}}
+        self.variants_212 = {'child': {'X_1097183_A_GG': self.homaltvardata},
+                             'mum': {'X_1097183_A_GG': self.hetvardata},
+                             'dad': {'X_1097183_A_GG': self.homaltvardata}}
+        self.variants_222 = {'child': {'X_1097183_A_GG': self.homaltvardata},
+                             'mum': {'X_1097183_A_GG': self.homaltvardata},
+                             'dad': {'X_1097183_A_GG': self.homaltvardata}}
+        self.variants_1 = {'child': {'X_1097183_A_GG': self.hetvardata}}
+        self.variants_2 = {'child': {'X_1097183_A_GG': self.homaltvardata}}
+
+        self.genes_hemizygous = {
+        '1234': {'chr': 'X', 'start': '1097083', 'end': '1098083',
+                 'symbol': 'DDX3X', 'status': {'Probable DD gene'},
+                 'mode': {'Hemizygous'},
+                 'mechanism': {'Loss of function'}}
+        }
+        self.genes_X_linked_dominant = copy.deepcopy(self.genes_hemizygous)
+        self.genes_X_linked_dominant['1234']['mode'] = 'X-linked dominant'
+        self.genes_X_linked_over_dominant = copy.deepcopy(self.genes_hemizygous)
+        self.genes_X_linked_over_dominant['1234']['mode'] = 'X-linked over-dominant'
+
+        self.child_XX = create_test_person('fam', 'child_id', 'dad_id', 'mum_id',
+                                        'XX', '2', '/vcf/path')
+        self.child_XXY = create_test_person('fam', 'child_id', 'dad_id',
+                                           'mum_id',
+                                           'XXY', '2', '/vcf/path')
+        self.child_XY = create_test_person('fam', 'child_id', 'dad_id',
+                                           'mum_id',
+                                           'XY', '2', '/vcf/path')
+        self.child_XX2 = create_test_person('fam', 'child_id', '0',
+                                           '0',
+                                           'XX', '2', '/vcf/path')
+        self.child_XY2 = create_test_person('fam', 'child_id', '0',
+                                            '0',
+                                            'XY', '2', '/vcf/path')
+        self.mum = create_test_person('fam', 'mum_id', '0', '0', 'XX', '1',
+                                      '/vcf/path')
+        self.mum_aff = create_test_person('fam', 'mum_id', '0', '0', 'XX', '2',
+                                          '/vcf/path')
+        self.dad = create_test_person('fam', 'dad_id', '0', '0', 'XY', '1',
+                                      '/vcf/path')
+        self.dad_aff = create_test_person('fam', 'dad_id', '0', '0', 'XY', '2',
+                                          '/vcf/path')
+
+        self.family_XX_both_unaff = create_test_family(self.child_XX, self.mum,
+                                                    self.dad)
+        self.family_XX_mum_aff = create_test_family(self.child_XX, self.mum_aff,
+                                                 self.dad)
+        self.family_XX_dad_aff = create_test_family(self.child_XX, self.mum,
+                                                 self.dad_aff)
+        self.family_XX_both_aff = create_test_family(self.child, self.mum_aff,
+                                                  self.dad_aff)
+        self.family_XX_no_parents = create_test_family(self.child_XX2, None, None)
+        self.family_XY_both_unaff = create_test_family(self.child_XY, self.mum,
+                                                       self.dad)
+        self.family_XY_mum_aff = create_test_family(self.child_XY, self.mum_aff,
+                                                    self.dad)
+        self.family_XY_dad_aff = create_test_family(self.child_XY, self.mum,
+                                                    self.dad_aff)
+        self.family_XY_both_aff = create_test_family(self.child_XY, self.mum_aff,
+                                                     self.dad_aff)
+        self.family_XY_no_parents = create_test_family(self.child_XY2, None,
+                                                       None)
 
     def test_get_variant_genotype(self):
         #test that variant genotype is calculated correctly from child chromosomal sex
+        pass
+
+    def test_allosomal_no_parents(self):
         pass
 
     def test_gn_hemizygous_gt_homozygous_parents_filter(self):
