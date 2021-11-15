8a03a96e9970b15bc639e6a35d3bb486ff15a749
RuthEberhardt <re3@sanger.ac.uk>
2021-11-15 14:35:13 +0000

    fix triogenotype test

--- tests/test_triogenotype.py
+++ tests/test_triogenotype.py
@@ -1,3 +1,26 @@
+"""
+Copyright (c) 2021 Genome Research Limited
+Author: Ruth Eberhardt <re3@sanger.ac.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 import unittest
 
 from tests.test_utils import create_test_snv
@@ -20,7 +43,9 @@ class TestTrioGenotypes(unittest.TestCase):
                          'max_af_pops': '.', 'ddd_af': '0', 'revel': '1',
                          'polyphen': '.', 'hgvsc': '.', 'hgvsp': '.',
                          'sex': 'XY', 'dnm': '.',
-                         'gt': '1/1', 'gq': '50'}
+                         'gt': '1/1', 'gq': '50', 'ac_XX': '2', 'ac_XY': '5',
+                         'nhomalt_XX': '379', 'nhomalt_XY': '4',
+                         'ddd_father_af': '.'}
         hetvardata = {'chrom': '1', 'pos': '100000', 'ref': 'A', 'alt': 'G',
                       'consequence': 'cq', 'ensg': 'ensg', 'symbol': 'KMTD2',
                       'feature': 'feature', 'canonical': 'YES',
@@ -28,7 +53,9 @@ class TestTrioGenotypes(unittest.TestCase):
                       'max_af_pops': '.', 'ddd_af': '0', 'revel': '1',
                       'polyphen': '.', 'hgvsc': '.', 'hgvsp': '.',
                       'sex': 'XY', 'dnm': '.',
-                      'gt': '0/1', 'gq': '50'}
+                      'gt': '0/1', 'gq': '50', 'ac_XX': '2', 'ac_XY': '5',
+                      'nhomalt_XX': '379', 'nhomalt_XY': '4',
+                      'ddd_father_af': '.'}
         homrefvardata = {'chrom': '1', 'pos': '100000', 'ref': 'A', 'alt': 'G',
                          'consequence': 'cq', 'ensg': 'ensg', 'symbol': 'KMTD2',
                          'feature': 'feature', 'canonical': 'YES',
@@ -36,7 +63,9 @@ class TestTrioGenotypes(unittest.TestCase):
                          'max_af_pops': '.', 'ddd_af': '0', 'revel': '1',
                          'polyphen': '.', 'hgvsc': '.', 'hgvsp': '.',
                          'sex': 'XY', 'dnm': '.',
-                         'gt': '0/0', 'gq': '50'}
+                         'gt': '0/0', 'gq': '50', 'ac_XX': '2', 'ac_XY': '5',
+                         'nhomalt_XX': '379', 'nhomalt_XY': '4',
+                         'ddd_father_af': '.'}
         self.homaltvar = create_test_snv(homaltvardata)
         self.hetvar = create_test_snv(hetvardata)
         self.homrefvar = create_test_snv(homrefvardata)
@@ -115,6 +144,7 @@ class TestTrioGenotypes(unittest.TestCase):
                         'dad': {}}
             add_trio_genotypes(family, variants)
             self.assertEqual(self.childvar.triogenotype, triostring)
+
 
 if __name__ == '__main__':
     unittest.main()
